53ec1f12ce2c40e8f97f37b5ed30e740e0d068ec
calexs <leopardotm@outlook.com>
2022-05-11 23:57:08 -0300

    (update): adding components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Feed from "./Feed";
+import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
   return (
-    <div className="App">
-      React App
+    <div className="app">
+      <Sidebar />
+      <Feed />
+      <Widgets />
     </div>
   );
 }
