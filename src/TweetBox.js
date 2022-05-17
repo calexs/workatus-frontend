2a6a6d669a8405a87168d56b0524521371f09576
calexs <leopardotm@outlook.com>
2022-05-17 19:58:52 -0300

    (update): removing firebase db from tweetbox

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,6 +1,6 @@
 import { Avatar, Button } from "@material-ui/core";
 import React, { useState } from "react";
-import db from "./firebase";
+// import db from "./firebase";
 import "./TweetBox.css";
 
 function TweetBox() {
@@ -10,15 +10,15 @@ function TweetBox() {
   const sendTweet = (e) => {
     e.preventDefault();
 
-    db.collection("posts").add({
-      username: "happystark",
-      displayName: "Atharva Deosthale",
-      avatar:
-        "https://scontent-bom1-1.xx.fbcdn.net/v/t1.0-1/c0.33.200.200a/p200x200/51099653_766820610355014_8315780769297465344_o.jpg?_nc_cat=101&_nc_sid=7206a8&_nc_ohc=c1qBHkwAgVsAX8KynKU&_nc_ht=scontent-bom1-1.xx&oh=340b05bea693dd1671296e0c2d004bb3&oe=5F84CA62",
-      verified: true,
-      text: tweetMessage,
-      image: tweetImage,
-    });
+    // db.collection("posts").add({
+    //   username: "happystark",
+    //   displayName: "Atharva Deosthale",
+    //   avatar:
+    //     "https://scontent-bom1-1.xx.fbcdn.net/v/t1.0-1/c0.33.200.200a/p200x200/51099653_766820610355014_8315780769297465344_o.jpg?_nc_cat=101&_nc_sid=7206a8&_nc_ohc=c1qBHkwAgVsAX8KynKU&_nc_ht=scontent-bom1-1.xx&oh=340b05bea693dd1671296e0c2d004bb3&oe=5F84CA62",
+    //   verified: true,
+    //   text: tweetMessage,
+    //   image: tweetImage,
+    // });
 
     setTweetMessage("");
     setTweetImage("");
