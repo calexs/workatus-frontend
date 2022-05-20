02dbf96da5aaacaf76fd6b3d955a0db75a01aa0d
calexs <leopardotm@outlook.com>
2022-05-20 08:53:59 -0300

    (add): auth.service.js for API authentication

---
+++ src/services/auth.service.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const API_URL = "";
+
+class AuthService {
+  async login(username, password) {
+    const res = await axios
+      .post(API_URL + "signin", {
+        username,
+        password
+      });
+    if (res.data.accesToken) {
+      localStorage.setItem("user", JSON.stringify(res.data));
+    }
+    return res.data;
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username, email, password) {
+    return axios
+      .post(API_URL + "signup", {
+        username,
+        email,
+        password
+      });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+}
+
+export default new AuthService();
