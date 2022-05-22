6b7fbf537141bf8b25a51ea9e44e29c56312d9b7
calexs <leopardotm@outlook.com>
2022-05-22 01:31:36 -0300

    (update): adding Login component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Feed from "./Feed";
 import Sidebar from "./Sidebar";
 import Widgets from "./Widgets";
+import Login from "./component/Login"
 
 function App() {
   return (
