c1fb607d77b1bb80151b8198c1eb9c62dc30e6ba
calexs <leopardotm@outlook.com>
2022-05-22 13:38:30 -0300

    (modify): Feed component switch hooks

--- src/Feed.js
+++ src/Feed.js
@@ -1,36 +1,86 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import "./Feed.css";
 import Post from "./Post";
 import TweetBox from "./TweetBox";
 // import db from "./firebase";
+import UserService from "./services/user.service";
 
-function Feed() {
-  const [posts, setPosts] = useState([]);
+export default class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "",
+      posts: ""
+    };
+  }
 
-  useEffect(() => {
-    // db.collection("posts").onSnapshot((snapshot) => {
-    //   setPosts(snapshot.docs.map((doc) => doc.data()));
-    // });
-  }, []);
+  componentDidMount() {
+    UserService.getPublicContent().then(
+      response => {
+        this.setState({
+          content: response.data
+        });
+      },
+      error => {
+        this.setState({
+          content:
+            (error.response && error.response.data) ||
+            error.message ||
+            error.toString()
+        });
+      }
+    );
+  }
 
-  return (
-    <div className="feed">
-      <div className="feed__header">
-        <h2>Home</h2>
+  render() {
+    return (
+      <div className="feed">
+        <div className="feed__header">
+          <h2>Home</h2>
+        </div>
+        <TweetBox />
+        {this.state.posts.map((post) => (
+          <Post
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
       </div>
-      <TweetBox />
-      {posts.map((post) => (
-        <Post
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
-        />
-      ))}
-    </div>
-  );
+    );
+  }
 }
 
-export default Feed;
+// function Feed() {
+//   const [posts, setPosts] = useState([]);
+
+//   useEffect(() => {
+//     db.collection("posts").onSnapshot((snapshot) => {
+//       setPosts(snapshot.docs.map((doc) => doc.data()));
+//     });
+//   }, []);
+
+//   return (
+//     <div className="feed">
+//       <div className="feed__header">
+//         <h2>Home</h2>
+//       </div>
+//       <TweetBox />
+//       {posts.map((post) => (
+//         <Post
+//           displayName={post.displayName}
+//           username={post.username}
+//           verified={post.verified}
+//           text={post.text}
+//           avatar={post.avatar}
+//           image={post.image}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Feed;
