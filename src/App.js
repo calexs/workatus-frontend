7b74d53894696a64a2b0561509a86517c703e5fc
calexs <leopardotm@outlook.com>
2022-05-23 20:40:16 -0300

    (update): importing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Feed from "./Feed";
 import Sidebar from "./Sidebar";
 import Widgets from "./Widgets";
-import Login from "./component/login.component"
+import AuthService from "./services/auth.service";
+import Login from "./component/login.component";
+import Profile from "./component/profile.component";
 
 function App() {
   return (
